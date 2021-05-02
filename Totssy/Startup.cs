3db6588603158f508ba07bb55c1ae076c090e040
islam <islam@DESKTOP-UDNN17H>
2021-05-02 03:15:53 +0200

    5231665

--- Totssy/Startup.cs
+++ Totssy/Startup.cs
@@ -27,6 +27,8 @@ namespace Totssy
             services.AddControllersWithViews();
             services.AddScoped<Iproduct, ProductRepos>();
             services.AddScoped<Iuser, UserRepos>();
+
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
